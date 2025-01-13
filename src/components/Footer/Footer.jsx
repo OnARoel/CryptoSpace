0622691a8db74fc55d49c38da70fd040fad6a8be
Andre Roel <roelandre96@gmail.com>
2025-01-12 19:04:10 -0500

    Added media queries for functionality

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2025 CryptoSpace</p>
+    </div>
+  )
+}
+
+export default Footer
